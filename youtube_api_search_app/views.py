b846f763fc4521e67606783457084c313731ea1f
Joshua Christian <joshua3christianx@gmail.com>
2020-09-11 00:57:02 +0100

    added the requirements file

--- youtube_api_search_app/views.py
+++ youtube_api_search_app/views.py
@@ -10,4 +10,4 @@ def search(request):
 
 # Create your views here.
 def search_result(request):
-    return render(request, "youtube_api_search_app/search_result.html")
+    return render(request, "search_result.html")
