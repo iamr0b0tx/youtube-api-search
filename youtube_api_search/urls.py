ad42dbdd4e51a4684c81e3915491268575609370
Joshua Christian <joshua3christianx@gmail.com>
2020-09-11 00:40:53 +0100

    created search template with html and bootstrap. edited app urls

--- youtube_api_search/urls.py
+++ youtube_api_search/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('youtube_api_search_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- youtube_api_search_app/urls.py
+++ youtube_api_search_app/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index),
-    path('search/', views.search),
-    path('search/result/', views.search_result),
+    path('', views.index, name="index"),
+    path('search/', views.search, name="search"),
+    path('search/result/', views.search_result, name="search_result"),
 ]
