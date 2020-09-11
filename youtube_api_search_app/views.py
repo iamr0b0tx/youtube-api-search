32def519ea4ae189dd898916cd7aacd7c2d4682b
Joshua Christian <joshua3christianx@gmail.com>
2020-09-11 12:16:56 +0100

    html template pages prepared

--- youtube_api_search_app/views.py
+++ youtube_api_search_app/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 
 # Create your views here.
 def index(request):
+    ''' returns the index page (in this case no index page exists so it redirects to the serach page) '''
     return HttpResponseRedirect(reverse(search))
 
 # Create your views here.
 def search(request):
+    ''' returns the html search page with an input field to receive query from user '''
     return render(request, "search.html")
 
 # Create your views here.
 def search_result(request):
+    ''' returns the search result from searching youtube api as html response '''
     context = {
         "search_query": "making up shopping",
         "result": [
@@ -17,7 +20,7 @@ def search_result(request):
                 "title": "Full face Drug",
                 "channel_name": "Christen Dominique",
                 "num_of_views": 441373,
-                "description": "Watch me on style code live Now and here at 9pm EST and 6pm EPN . here is my drug store vs the second drug store where we whatever"
+                "description": "Watch me on style code live Now and here at 9pm EST and 6pm EPN . here is my drug store vs the second drug store where we whatever",
             },
             {
                 "title": "Half Full face Drug",
