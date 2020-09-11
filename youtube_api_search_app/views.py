63295d184eb956337f6804c1893601bbefc37044
Joshua Christian <joshua3christianx@gmail.com>
2020-09-11 10:47:59 +0100

    updated search result html, and search html. new favicon. added dummy data to view

--- youtube_api_search_app/views.py
+++ youtube_api_search_app/views.py
@@ -10,4 +10,27 @@ def search(request):
 
 # Create your views here.
 def search_result(request):
-    return render(request, "search_result.html")
+    context = {
+        "search_query": "making up shopping",
+        "result": [
+            {
+                "title": "Full face Drug",
+                "channel_name": "Christen Dominique",
+                "num_of_views": 441373,
+                "description": "Watch me on style code live Now and here at 9pm EST and 6pm EPN . here is my drug store vs the second drug store where we whatever"
+            },
+            {
+                "title": "Half Full face Drug",
+                "channel_name": "Christen Dominique",
+                "num_of_views": 441373,
+                "description": "Watch me on style code live Now and here at 9pm EST and 6pm EPN . here is my drug store vs the second drug store where we whatever"
+            },
+            {
+                "title": "Almost Full face Drug",
+                "channel_name": "Christen Dominique",
+                "num_of_views": 441373,
+                "description": "Watch me on style code live Now and here at 9pm EST and 6pm EPN . here is my drug store vs the second drug store where we whatever"
+            },
+        ]
+    }
+    return render(request, "search_result.html", context)
