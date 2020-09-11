4991bb206a4038af9906df15022c63e7098dd1e7
Joshua Christian <joshua3christianx@gmail.com>
2020-09-11 10:48:32 +0100

    'django.contrib.humanize' added to installed apps

--- youtube_api_search/settings.py
+++ youtube_api_search/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     # 'django.contrib.sessions',
     # 'django.contrib.messages',
     # 'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'youtube_api_search_app',
+
 ]
 
 MIDDLEWARE = [
